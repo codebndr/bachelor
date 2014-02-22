789787a8454efda4f3bbb3f52eee288e004d76b8
ekoedmedia <ekoedmedia@gmail.com>
2014-02-22 14:26:34 -0700

    Removed User Registration from Walkthrough Removed no longer needed tests for About, Tech, Team, etc. pages

--- Symfony/src/Codebender/StaticBundle/Tests/Controller/DefaultControllerFunctionalTest.php
+++ Symfony/src/Codebender/StaticBundle/Tests/Controller/DefaultControllerFunctionalTest.php
@@ -6,95 +6,6 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class DefaultControllerFunctionalTest extends WebTestCase
 {
-	public function testAboutAction_Generic()
-	{
-		$client = static::createClient();
-
-		$crawler = $client->request('GET', '/static/about');
-
-		$this->assertEquals(1, $crawler->filter('html:contains("we help you write and share code")')->count());
-		$this->assertEquals(1, $crawler->filter('html:contains("Do It Together")')->count());
-		$this->assertEquals(1, $crawler->filter('html:contains("Cross-platform")')->count());
-		$this->assertEquals(1, $crawler->filter('html:contains("From DIY to DIT. Share and Collaborate.")')->count());
-		$this->assertGreaterThanOrEqual(3, $crawler->filter('h3')->count());
-		$this->assertGreaterThanOrEqual(3, $crawler->filter('h4')->count());
-	}
-
-	public function testAboutAction_LoggedIn()
-	{
-		$client = static::createClient(array(), array(
-			'PHP_AUTH_USER' => 'tester',
-			'PHP_AUTH_PW' => 'testerPASS',
-		));
-
-		$crawler = $client->request('GET', '/static/about');
-		$this->assertEquals(1, $crawler->filter('html:contains("we help you write and share code")')->count());
-		$this->assertEquals(0, $crawler->filter('input[value=Register]')->count());
-	}
-
-	public function testAboutAction_Anonymous()
-	{
-		$client = static::createClient();
-
-		$crawler = $client->request('GET', '/static/about');
-
-		$this->assertEquals(1, $crawler->filter('html:contains("Sign Up")')->count());
-		$this->assertEquals(1, $crawler->filter('html:contains("Start coding in minutes.")')->count());
-		$this->assertEquals(1, $crawler->filter('html:contains("Hi! Why don\'t you sign up for a codebender account?")')->count());
-		$this->assertEquals(1, $crawler->filter('input[value=Register]')->count());
-        $this->assertEquals("Username", $crawler->filter('input')->eq(1)->attr('placeholder'));
-        $this->assertEquals("Email", $crawler->filter('input')->eq(2)->attr('placeholder'));
-        $this->assertEquals("Type a password", $crawler->filter('input')->eq(3)->attr('placeholder'));
-        $this->assertEquals("Repeat password", $crawler->filter('input')->eq(4)->attr('placeholder'));
-	}
-
-	public function testTechAction()
-	{
-		$client = static::createClient();
-
-		$crawler = $client->request('GET', '/static/tech');
-
-		$this->assertEquals(1, $crawler->filter('html:contains("Cloud IDE")')->count());
-		$this->assertEquals(1, $crawler->filter('html:contains("Makers\' Hub")')->count());
-		$this->assertEquals(1, $crawler->filter('html:contains("Documentation and Suggestions")')->count());
-		$this->assertEquals(1, $crawler->filter('html:contains("under the hood")')->count());
-		$this->assertEquals(1, $crawler->filter('html:contains("Open Source")')->count());
-		$this->assertEquals(1, $crawler->filter('html:contains("Ariadne Bootloader")')->count());
-		$this->assertGreaterThanOrEqual(2, $crawler->filter('h1')->count());
-		$this->assertGreaterThanOrEqual(11, $crawler->filter('h3')->count());
-		$this->assertGreaterThanOrEqual(12, $crawler->filter('h4')->count());
-	}
-
-	public function testTeamAction()
-	{
-		$client = static::createClient();
-
-		$crawler = $client->request('GET', '/static/team');
-
-		$this->assertEquals(1, $crawler->filter('html:contains("Vasilis Georgitzikis")')->count());
-		$this->assertEquals(1, $crawler->filter('html:contains("Stelios Tsampas")')->count());
-		$this->assertEquals(1, $crawler->filter('html:contains("Dimitris Amaxilatis")')->count());
-		$this->assertEquals(1, $crawler->filter('html:contains("Maria Kousta")')->count());
-		$this->assertEquals(1, $crawler->filter('html:contains("Markellos Orfanos")')->count());
-		$this->assertEquals(1, $crawler->filter('html:contains("Dimitris Dimakopoulos")')->count());
-		$this->assertEquals(1, $crawler->filter('html:contains("Dimitrios Christidis")')->count());
-		$this->assertEquals(1, $crawler->filter('html:contains("Alexandros Baltas")')->count());
-		$this->assertGreaterThanOrEqual(8, $crawler->filter('h2')->count());
-		$this->assertGreaterThanOrEqual(3, $crawler->filter('h1')->count());
-	}
-
-	public function testTutorialsAction()
-	{
-		$client = static::createClient();
-
-		$crawler = $client->request('GET', '/static/tutorials');
-
-		$this->assertEquals(1, $crawler->filter('html:contains("Learn how to use codebender")')->count());
-		$this->assertEquals(1, $crawler->filter('html:contains("Walkthrough Video!")')->count());
-		$this->assertEquals(1, $crawler->filter('html:contains("Plugin Tutorial!")')->count());
-		$this->assertGreaterThanOrEqual(1, $crawler->filter('h1')->count());
-		$this->assertGreaterThanOrEqual(2, $crawler->filter('h3')->count());
-	}
 
 	public function testWalkthroughAction_ValidPages()
 	{
@@ -146,10 +57,7 @@ class DefaultControllerFunctionalTest extends WebTestCase
 		$client = static::createClient();
 
 		$crawler = $client->request('GET', '/static/walkthrough/page/5');
-		$this->assertEquals(1, $crawler->filter('html:contains("Sign Up")')->count());
-		$this->assertEquals(1, $crawler->filter('html:contains("Start coding in minutes.")')->count());
-		$this->assertEquals(1, $crawler->filter('html:contains("Hi! Why don\'t you sign up for a codebender account?")')->count());
-		$this->assertEquals(1, $crawler->filter('input[value=Register]')->count());
+		$this->assertEquals(1, $crawler->filter('html:contains("It\'s quite simple really.")')->count());
 	}
 
 	public function testWalkthroughAction_Invalid()
@@ -178,86 +86,6 @@ class DefaultControllerFunctionalTest extends WebTestCase
 		$this->assertGreaterThanOrEqual(2, $crawler->filter('h3')->count());
 	}
 
-	public function testPartnerAction_Arno()
-	{
-		$client = static::createClient();
-
-		$crawler = $client->request('GET', '/static/partner/arno');
-
-		$this->assertEquals(1, $crawler->filter('html:contains("Learning the basics of electronics and programming is challenging")')->count());
-	}
-
-	public function testPartnerAction_Invalid()
-	{
-		$client = static::createClient();
-
-		$client->request('GET', '/static/partner/invalid');
-		$this->assertTrue($client->getResponse()->isRedirect());
-	}
-
-	public function testInfoPointsAction()
-	{
-		$client = static::createClient(array(), array(
-			'PHP_AUTH_USER' => 'tester',
-			'PHP_AUTH_PW' => 'testerPASS',
-		));
-
-		$crawler = $client->request('GET', '/static/info/points');
-		$this->assertEquals(1, $crawler->filter('html:contains("?referrer=tester")')->count());
-		$this->assertEquals(1, $crawler->filter('html:contains("Do good. Get T-Shirts!")')->count());
-	}
-
-	public function testInfoKarmaAction()
-	{
-		$client = static::createClient();
-
-		$crawler = $client->request('GET', '/static/info/karma');
-
-		$this->assertEquals(1, $crawler->filter('html:contains("What is karma?")')->count());
-		$this->assertEquals(1, $crawler->filter('html:contains("Karma in codebender")')->count());
-	}
-
-    public function testInfoPrivateProjectAction_notLoggedIn()
-    {
-        $client = static::createClient();
-
-        $crawler = $client->request('GET', '/static/info/private_projects');
-
-        $this->assertEquals(1, $crawler->filter('html:contains("Private Projects")')->count());
-        $this->assertEquals(1, $crawler->filter('html:contains("We\'re really excited to announce our first premium feature")')->count());
-        $this->assertEquals(0, $crawler->filter('html:contains("Currently Available")')->count());
-    }
-
-    public function testInfoPrivateProjectAction_LoggedInWithPrivate()
-    {
-        $client = static::createClient(array(), array(
-            'PHP_AUTH_USER' => 'tester',
-            'PHP_AUTH_PW' => 'testerPASS',
-        ));
-
-        $crawler = $client->request('GET', '/static/info/private_projects');
-
-        $this->assertEquals(1, $crawler->filter('html:contains("Private Projects")')->count());
-        $this->assertEquals(1, $crawler->filter('html:contains("We\'re really excited to announce our first premium feature")')->count());
-        $this->assertEquals(1, $crawler->filter('html:contains("Currently Available")')->count());
-        $this->assertEquals(1, $crawler->filter('html:contains("never")')->count());
-    }
-
-    public function testInfoPrivateProjectAction_LoggedInWithoutPrivate()
-    {
-        $client = static::createClient(array(), array(
-            'PHP_AUTH_USER' => 'testacc',
-            'PHP_AUTH_PW' => 'testaccPWD',
-        ));
-
-        $crawler = $client->request('GET', '/static/info/private_projects');
-
-        $this->assertEquals(1, $crawler->filter('html:contains("Private Projects")')->count());
-        $this->assertEquals(1, $crawler->filter('html:contains("We\'re really excited to announce our first premium feature")')->count());
-        $this->assertEquals(1, $crawler->filter('html:contains("Currently Available")')->count());
-        $this->assertEquals(0, $crawler->filter('html:contains("never")')->count());
-    }
-
     public function testUploadBootloaderAction()
     {
         $client = static::createClient();
@@ -265,7 +93,7 @@ class DefaultControllerFunctionalTest extends WebTestCase
         $crawler = $client->request('GET', '/static/upload/bootloader');
 
         $this->assertEquals(1, $crawler->filter('html:contains("Upload Bootloader")')->count());
-        $this->assertEquals(1, $crawler->filter('html:contains("Please select your programmer, device and usb port from the lists bellow and click start the begin the upload.")')->count());
+        $this->assertEquals(1, $crawler->filter('html:contains("Please select your programmer, device and usb port from the lists below and click start the begin the upload.")')->count());
     }
 
 }
