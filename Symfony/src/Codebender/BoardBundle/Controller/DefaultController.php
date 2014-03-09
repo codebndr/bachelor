daa6d3b45b33a14568bfbb78544a53667d8ebe5b
ekoedmedia <ekoedmedia@gmail.com>
2014-03-09 13:18:56 -0600

    Added Comments and changed JSON Response to be valid (Code Cleanup)

--- Symfony/src/Codebender/BoardBundle/Controller/DefaultController.php
+++ Symfony/src/Codebender/BoardBundle/Controller/DefaultController.php
@@ -10,14 +10,34 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Security\Core\SecurityContext;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
-
 class DefaultController extends Controller
 {
 
+    /**
+     * @var Doctrine\ORM\EntityManager
+     */
     protected $em;
+
+    /**
+     * @var Symfony\Component\Security\Core\SecurityContext
+     */
     protected $sc;
+
+    /**
+     * @var Symfony\Component\DependencyInjection\ContainerInterface
+     */
     protected $container;
 
+    /**
+     * Creates Personal Board
+     * 
+     * @param Integer $owner
+     * @param String $description
+     * @param DateTime $starts
+     * @param DateTime $expires
+     * @param Integer $number
+     * @return JSON encoded Response
+     */
     public function createBoardsPlanAction($owner, $description, $starts, $expires, $number)
     {
         $plan = new PersonalBoards();
@@ -31,9 +51,16 @@ class DefaultController extends Controller
         $this->em->persist($plan);
         $this->em->flush();
 
-        return new Response(json_encode(array("success" => true, "id" => $plan->getId())));
+        $response = new Response(json_encode(array("success" => true, "id" => $plan->getId()));
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
     }
 
+    /**
+     * Lists boards
+     * 
+     * @return JSON encoded Response
+     */
 	public function listAction()
 	{
 		$boards = array();
@@ -79,13 +106,23 @@ class DefaultController extends Controller
 		return $response;
 	}
 
+    /**
+     * Edits a Boards name and description 
+     *
+     * @param Integer $id
+     * @param String $name
+     * @param String $description
+     * @return JSON encoded Response
+     */
     public function editAction($id, $name, $description)
     {
         $board = $this->getBoardById($id);
         $perm = json_decode($this->checkBoardPermissions($id), true);
         if(!$perm['success'])
         {
-            return new Response(json_encode(array("success" => false, "message" => "Cannot edit board '".$board->getName()."'.")));
+            $response = new Response(json_encode(array("success" => false, "message" => "Cannot edit board '".$board->getName()."'.")));
+            $response->headers->set('Content-Type', 'application/json');
+            return $response;
         }
         $board->setName($name);
         $board->setDescription($description);
@@ -94,39 +131,72 @@ class DefaultController extends Controller
         $em->persist($board);
         $em->flush();
 
-        return new Response(json_encode(array("success" => true, "new_name" => $name, "new_desc" => $description)));
+        $response = new Response(json_encode(array("success" => true, "new_name" => $name, "new_desc" => $description)));
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
     }
 
+    /**
+     * Sets name of Board 
+     *
+     * @param Integer $id
+     * @param String $description
+     * @return JSON encoded Response
+     */
     public function setNameAction($id, $name)
     {
         $board = $this->getBoardById($id);
         $perm = json_decode($this->checkBoardPermissions($id), true);
         if(!$perm['success'])
         {
-            return new Response(json_encode(array("success" => false, "message" => "Cannot set name for board '".$board->getName()."'.")));
+            $response = new Response(json_encode(array("success" => false, "message" => "Cannot set name for board '".$board->getName()."'.")));
+            $response->headers->set('Content-Type', 'application/json');
+            return $response;
         }
         $board->setName($name);
         $em = $this->em;
         $em->persist($board);
         $em->flush();
-        return new Response(json_encode(array("success" => true)));
+
+        $response = new Response(json_encode(array("success" => true)));
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
     }
 
+    /**
+     * Sets description of Board
+     *
+     * @param Integer $id
+     * @param String $description
+     * @return JSON encoded Response
+     */
     public function setDescriptionAction($id, $description)
     {
         $board = $this->getBoardById($id);
         $perm = json_decode($this->checkBoardPermissions($id), true);
         if(!$perm['success'])
         {
-            return new Response(json_encode(array("success" => false, "message" => "Cannot set description for board '".$board->getName()."'.")));
+            $response = new Response(json_encode(array("success" => false, "message" => "Cannot set description for board '".$board->getName()."'.")));
+            $response->headers->set('Content-Type', 'application/json');
+            return $response;
         }
         $board->setDescription($description);
         $em = $this->em;
         $em->persist($board);
         $em->flush();
-        return new Response(json_encode(array("success" => true)));
+
+        $response = new Response(json_encode(array("success" => true)));
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
     }
 
+    /**
+     * Adds a Board given a User and Board Array 
+     *
+     * @param Array $b
+     * @param Integer $user_id
+     * @return JSON encoded Response
+     */
     public function addBoardAction($b, $user_id)
     {
         $owner = $this->em->getRepository('CodebenderUserBundle:User')->find($user_id);
@@ -141,30 +211,50 @@ class DefaultController extends Controller
 
         $this->em->persist($board);
         $this->em->flush();
-        return new Response(json_encode(array("success" => true)));
+
+        $response = new Response(json_encode(array("success" => true)));
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
 
     }
 
+    /**
+     * Checks if Board is Valid 
+     *
+     * @param Array $b
+     * @return JSON encoded Response
+     */
     public function isValidBoardAction($b)
     {
         if(isset($b['name']) && isset($b["upload"]) && isset($b["bootloader"]) && isset($b["build"]))
         {
-            return new Response(json_encode(array("success" => true)));
+            $success = true;
         }
         else
         {
-            return new Response(json_encode(array("success" => false)));
-
+            $success = false;
         }
+
+        $response = new Response(json_encode(array("success" => $success)));
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
     }
 
+    /**
+     * Deletes a Board 
+     * 
+     * @param Integer $id
+     * @return JSON encoded Response
+     */
     public function deleteBoardAction($id)
     {
         $perm = json_decode($this->checkBoardPermissions($id), true);
 
         if(!($perm['success']))
         {
-            return new Response(json_encode(array("success" => false, "message" => "You have no permissions to delete this board.")));
+            $response = new Response(json_encode(array("success" => false, "message" => "You have no permissions to delete this board.")));
+            $response->headers->set('Content-Type', 'application/json');
+            return $response;
         }
 
         $board = $this->getBoardById($id);
@@ -172,15 +262,31 @@ class DefaultController extends Controller
         $this->em->remove($board);
         $this->em->flush();
 
-        return new Response(json_encode(array("success" => true, "message" => "Board '".$board->getName()."' was successfully deleted.")));
+        $response = new Response(json_encode(array("success" => true, "message" => "Board '".$board->getName()."' was successfully deleted.")));
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
 
     }
 
+    /**
+     * Checks if user can add Personal Boards
+     *
+     * @param Integer $user_id
+     * @return JSON encoded Response
+     */
     public function canAddPersonalBoardAction($user_id)
     {
-        return new Response($this->canAddPersonalBoard($user_id));
+        $response = new Response($this->canAddPersonalBoard($user_id));
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
     }
 
+    /**
+     * Parses properties of Boards
+     *
+     * @param String $txtProperties
+     * @return JSON encoded Response board properties
+     */
     public function parsePropertiesFileAction($txtProperties) {
         $result = array();
         $lines = explode("\n", $txtProperties);
@@ -218,9 +324,17 @@ class DefaultController extends Controller
             unset($lines[$i]);
         }
 
-        return new Response(json_encode(array("success" => true, "boards" => $result)));
+        $response = new Response(json_encode(array("success" => true, "boards" => $result)));
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
     }
 
+    /**
+     * Checks if user can add Personal Boards
+     *
+     * @param Integer $user_id
+     * @return JSON encoded permission level
+     */
     protected function canAddPersonalBoard($user_id)
     {
         $boards = $this->em->getRepository('CodebenderBoardBundle:Board')->findByOwner($user_id);
@@ -240,21 +354,36 @@ class DefaultController extends Controller
             return json_encode(array("success" => false, "error" => "Cannot add personal board."));
         else
             return json_encode(array("success" => true, "available" => $maxPersonal - $currentPersonal));
-
     }
 
+    /**
+     * Checks Board Permissions based on an ID 
+     *
+     * @param Integer $id
+     * @return JSON encoded permission level
+     */
     protected function checkBoardPermissions($id)
     {
         $board = $this->getBoardById($id);
         $current_user = $this->sc->getToken()->getUser();
 
         if($board->getOwner()!== null && $current_user !== "anon." && $current_user->getId() === $board->getOwner()->getId())
-            return json_encode(array("success" => true));
+            $returnVal = array("success" => true);
         else
-            return json_encode(array("success" => false, "error" => "You have no permissions for this board.", "id" => $id));
+            $returnVal = array("success" => false, "error" => "You have no permissions for this board.", "id" => $id);
+
+        $response = new Response(json_encode($returnVal));
+        $response->headers->set('Content-Type', 'application/json');
+        return $response;
 
     }
 
+    /**
+     * Gets a board from DB based on its ID
+     *
+     * @param Integer $id
+     * @return Board Entity
+     */
     protected function getBoardById($id)
     {
         $em = $this->em;
@@ -265,7 +394,13 @@ class DefaultController extends Controller
         return $board;
     }
 
-
+    /**
+     * Construction of Controller
+     *
+     * @param EntityManager $entityManager
+     * @param SecurityContext $securityContext
+     * @param ContainerInterface $container
+     */
     public function __construct(EntityManager $entityManager, SecurityContext $securityContext, ContainerInterface $container)
     {
         $this->em = $entityManager;
