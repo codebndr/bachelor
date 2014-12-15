978cadc2f4b11e1d7e9028306b7551b870d7c177
Vasileios Georgitzikis <tzikis@gmail.com>
2014-12-15 23:10:19 +0200

    ok, i think i got it now

--- scripts/install.sh
+++ scripts/install.sh
@@ -94,6 +94,7 @@ sudo chown -R `whoami`:$HTTPDUSER /opt/codebender/files
 
 # TODO: find a better way to generate this
 touch app/config/parameters.yml
+echo "parameters:" >> app/config/parameters.yml
 echo "    directory: '/opt/codebender/files'" >> app/config/parameters.yml
 echo "    database_path: '/opt/codebender/codebender.sqlite'" >> app/config/parameters.yml
 
